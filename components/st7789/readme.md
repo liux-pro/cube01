1d014ba0094b485840f2d6d80c736c891dc19ecc
Legend <me@liux.pro>
2023-01-20 01:29:58 +0800

    remove st7789 3rd-party driver

--- components/st7789/readme.md
+++
@@ -1,3 +0,0 @@
-# ST7789 driver
-Code from https://github.com/nopnop2002/esp-idf-st7789
-idf.py save-defconfig
--- components/st7789/st7789_faster.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_ST7789_FASTER_H_
-#define MAIN_ST7789_FASTER_H_
-
-#include "st7789.h"
-
-void lcdPrepareMultiPixels(TFT_t *dev);
-
-#endif /* MAIN_ST7789_FASTER_H_ */
